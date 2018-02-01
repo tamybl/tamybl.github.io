f671b57bc7b26e54bb27d821f5094f07a5bbd85d
tamybl <tamara.bravo.l@gmail.com>
2018-02-01 00:26:05 -0300

    Modificaciones de estilo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
+
+    $(window).resize(function() {
+        var bodyheight = $(this).height();
+        $("#home").height(bodyheight);
+    }).resize();
 });
