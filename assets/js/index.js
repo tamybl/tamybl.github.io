81f00aa8b10e9b6727e11e50a62d578cbf66e3ba
tamybl <tamara.bravo.l@gmail.com>
2018-02-01 00:27:14 -0300

    Añadiendo contenido, arreglando centrado texto seccion home

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 
     $(window).resize(function() {
-        var bodyheight = $(this).height();
-        $("#home").height(bodyheight);
+        var bodyHeight = $(this).height();
+        var menuHeight = $(".nav-menu").height();
+        $("#home").height(bodyHeight);
+        $(".flex").height(bodyHeight-menuHeight);
     }).resize();
 });
