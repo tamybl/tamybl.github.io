a69f498909a8c60255f82796a8335ad1792f95d0
tamybl <tamara.bravo.l@gmail.com>
2018-02-11 13:14:35 -0300

    Creando funcion de animacion de secciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+  var menuHeight = $(".nav-menu").height();
+   $('[data-toggle="tooltip"]').tooltip(); 
 
-    $(window).resize(function() {
-        var bodyHeight = $(this).height();
-        var menuHeight = $(".nav-menu").height();
-        $("#home").height(bodyHeight);
-        $(".flex").height(bodyHeight-menuHeight);
-    }).resize();
+  $(window).resize(function() {
+    var bodyHeight = $(this).height();
+    $("#home").height(bodyHeight);
+    $(".flex").height(bodyHeight-menuHeight);
+  }).resize();
+
+  // Animaciones secciones Sitio
+  $(document).scroll(function () {
+    animatedSection("#about", "animated fadeInDown", menuHeight, ".ion-ios-person-outline");
+    
+  })
+
+
+
+
+  // Texto en consola imagen introduccion
   var typed = new Typed("#typed", {
     stringsElement: '#typed-strings',
     typeSpeed: 0,
@@ -15,7 +26,23 @@ $(document).ready(function(){
     startDelay: 1000,
     loop: false
   }); 
-
-
 });
+
+/* Funcion que recibe parametros de posicion de un ID, clases, tamaño de menu 
+* y margen y agrega los atributos al llegar a la ubicacion especificada.
+*/
+function animatedSection(id, attribute, menu, classMenu) {
+  var scrollPosition = $(document).scrollTop();
+  var sectionTop = $(id).offset().top;
+  var sectionBottom = $(id).offset().top + $(id).height();
+  console.log(menu);
+  if(sectionTop <= scrollPosition+menu+50 && sectionBottom >= scrollPosition+menu) {
+    //console.log('Estoy en la seccion');
+    $(classMenu).parent().addClass("hover");
+  }
+  else {
+    $(classMenu).parent().removeClass("hover");
+    //console.log('No estoy en la seccion');
+  }
+}
 
