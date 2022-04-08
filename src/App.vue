6e4d5ceefedf884c5831b6e859c7deae84c98e77
Tamara Bravo <tamara.bravo.l@gmail.com>
2022-04-08 17:37:13 -0400

    Se garega compontente loader

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
+    <Loader />
     <nav-bar></nav-bar>
     <Header />
   </div>
 </template>
 
 <script>
+import Loader from "./components/Loader.vue";
 import Header from "./components/Header.vue";
 import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "App",
   components: {
+    Loader,
     NavBar,
     Header,
   },
+  data() {
+    return {};
+  },
+  mounted() {},
 };
 </script>
 
